8af3f9f05ac58d8708aaea983466baf23e260080
clayton-scherer <cscherer93@aol.com>
2019-07-07 18:02:50 -0400

    gh pages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const Events = require("./utils/Events");
-
+const ghpages = require('gh-pages');
+ 
 const events = new Events();
+
+ghpages.publish('dist', function(err) {});
 
 events.toggleSideNav();
 events.showTab();
